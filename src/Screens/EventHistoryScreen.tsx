0f7ec8e5a3ec449e0d329dddf96a36a57f7448a0
Gianmattia Gherardi <giamalfred@gmail.com>
2025-12-13 13:49:51 +0100

    Removed CameraManager.tsx and blanked EventHistoryScreen (kept it for future use)

--- src/Screens/EventHistoryScreen.tsx
+++ src/Screens/EventHistoryScreen.tsx
@@ -1,15 +1,7 @@
 import React from 'react-native';
-import CameraComponent from '../lib/CameraManager';
-import { PhotoFile } from 'react-native-vision-camera';
 
 const EventHistoryScreen = () => {
-    const usePhoto = (photo: PhotoFile) => {
-        console.log("The photo has been saved to: ", photo.path);
-    }
-
-    return (
-        <CameraComponent assignPhoto={usePhoto} />
-    )
+    return null;
 };
 
 export default EventHistoryScreen;
